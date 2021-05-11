6f0fa00c894f3ab3420ec6f6e2d1504f886c55ac
example <eren_gs03@hotmail.com>
2021-05-11 19:37:20 +0300

    (Java + React) Yazılımcı Geliştirme Kursu 5.Gün 3.Ödev

---
+++ Hrms/src/main/java/kodlamaio/Hrms/api/controllers/JobPositionController.java
@@ -0,0 +1,27 @@
+package kodlamaio.Hrms.api.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.Hrms.business.abstracts.JobPositionService;
+import kodlamaio.Hrms.entities.concretes.JobPosition;
+
+@RestController
+@RequestMapping("/api/jobpositions")
+public class JobPositionController {
+
+	   private JobPositionService jobPositionService;
+
+	public JobPositionController(JobPositionService jobPositionService) {
+		super();
+		this.jobPositionService = jobPositionService;
+	}
+	
+	@GetMapping("/getall")
+	public List<JobPosition> getAll(){
+    	return this.jobPositionService.getAll();
+    }
+}
---
+++ Hrms/src/main/java/kodlamaio/Hrms/entities/concretes/JobPosition.java
@@ -0,0 +1,35 @@
+package kodlamaio.Hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+
+@Table(name = "job_position")
+public class JobPosition {
+	
+	
+	        @Id
+	        @GeneratedValue
+	        @Column(name = "id")
+            private int id;
+	        
+            @Column(name = "position_name")
+            private String position_name;
+            
+            public JobPosition() {
+            	
+            }
+            
+			public JobPosition(int id, String position_name) {
+				super();
+				this.id = id;
+				this.position_name = position_name;
+			}
+}
